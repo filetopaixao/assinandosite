c292d51b753a46539e67898b93be1a5cbeabe54e
Fileto Filho <fileto.filho@voidsoftware.com>
2023-10-18 18:11:33 -0300

    add portfolio

--- src/components/sections/SectionTeam.js
+++ src/components/sections/SectionTeam.js
@@ -7,11 +7,15 @@
 
 import { useRef, useState, useEffect } from 'react';
 import { Slide } from '@mui/material';
-import TeamSlider from "../elements/TeamSlider";
 import { useOnScreen } from '../../utils/useOnScreen';
+import Portfolio1 from '../../assets/images/portfolio1.png';
+import Portfolio2 from '../../assets/images/portfolio2.png';
+import Portfolio3 from '../../assets/images/portfolio3.png';
+import Portfolio4 from '../../assets/images/portfolio4.png';
 
 const SectionTeam = () => {
 	const [isVisibleState, setIsVisibleState] = useState(false);
+	const [customersCount, setCustomersCount] = useState(0);
 
 	const visibilityRef = useRef(null);
 	
@@ -21,16 +25,37 @@ const SectionTeam = () => {
 		if(!isVisibleState && isVisible) setIsVisibleState(isVisible);
 	}, [isVisible, isVisibleState]);
 
+    useEffect(() => {
+        if (customersCount < 70 && isVisibleState) {
+            setTimeout(() => {
+                setCustomersCount(prevState => prevState + 1);
+            }, "25");
+        }
+    },[customersCount, isVisibleState]);
+
 	return (
 		<section className="section-team">
 			<div className="section-team__wrap">
 				<div className="section-team__wrap__header">
+                <h1>Mais de {customersCount} clientes!</h1>
 				<Slide direction="down" in={isVisibleState} style={{ transitionDelay: isVisibleState ? '500ms' : '0ms' }}>
-					<h1>Quem Somos</h1>
+					<h2 className="primary">Exemplos de alguns dos nossos trabalhos</h2>
 				</Slide>
 				</div>
-				<div className="section-team__wrap__members" ref={visibilityRef}>
-					<TeamSlider animationStart={isVisibleState} />
+				<div className="section-team__wrap__portfólio" ref={visibilityRef}>
+                    <div className="section-team__wrap__portfólio__image">
+                        <img src={Portfolio1} alt="Assiando site - Portfólio 1" />
+                    </div>
+                    <div className="section-team__wrap__portfólio__image">
+                        <img src={Portfolio2} alt="Assiando site - Portfólio 2" />
+                    </div>
+                    <div className="section-team__wrap__portfólio__image">
+                        <img src={Portfolio3} alt="Assiando site - Portfólio 3" />
+                    </div>
+                    <div className="section-team__wrap__portfólio__image">
+                        <img src={Portfolio4} alt="Assiando site - Portfólio 4" />
+                    </div>
+					{/*<TeamSlider animationStart={isVisibleState} />*/}
 				</div>
 			</div>
 		</section>
